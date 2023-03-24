c5adb022a6904ef7210fc2d8329dcbbc7e93fd1c
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-03-24 06:42:22 +0100

    Add project files.

---
+++ I AM SPEED (3 på rad EDITION)/Model/Game.cs
@@ -0,0 +1,105 @@
+﻿namespace I_AM_SPEED__3_på_rad_EDITION_.Model
+{
+    internal class Game
+    {
+        private string[] board = new string[9];
+        private string curentPlayer = "O";
+        private bool gameOver = false;
+
+        public Game()
+        {
+            for (int i = 0; i < board.Length; i++) { board[i] = " "; }
+        }
+
+        public void StartGame()
+        {
+            drawBoard();
+            gameLoop();
+        }
+
+        private void gameLoop()
+        {
+            swapPlayer();
+            userSelectSquare();
+            drawBoard();
+            Console.WriteLine("Draw er " + draw());
+            if (!doWeHaveAWINNER() || !draw())
+            {
+                gameLoop();
+            }
+            else
+            {
+                if (draw()) Console.WriteLine("Det blie uavgjort XD");
+                else Console.WriteLine("Gratulerrer " + curentPlayer);
+            }
+        }
+
+        private bool draw()
+        {
+            bool draw = true;
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i] == " ") { draw = false; break; }
+            }
+            return draw;
+        }
+
+        private bool doWeHaveAWINNER()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                // Loddrett
+                if (board[i] != " " && board[i] == board[i + 3] && board[i + 3] == board[i + 6]) { return true; }
+
+                // Vann rett
+                int offset = i * 3;
+                if (board[offset] != " " && board[offset] == board[offset + 1] && board[offset + 1] == board[offset + 2]) { return true; }
+            }
+
+            // Diagonalen
+            if (board[0] != " " && board[0] == board[4] && board[4] == board[8]) { return true; }
+            if (board[2] != " " && board[2] == board[4] && board[4] == board[6]) { return true; }
+
+            return false;
+        }
+
+        private void swapPlayer()
+        {
+
+            if (curentPlayer == "X") { curentPlayer = "O"; }
+            else { curentPlayer = "X"; }
+        }
+
+        private void drawBoard()
+        {
+            Console.WriteLine($"{board[0]}{board[1]}{board[2]}");
+            Console.WriteLine($"{board[3]}{board[4]}{board[5]}");
+            Console.WriteLine($"{board[6]}{board[7]}{board[8]}");
+        }
+
+        private void userSelectSquare()
+        {
+            string userInput = Console.ReadLine();
+            if (oneToNine(userInput) && userInput.Length == 1 && board[Int16.Parse(userInput) - 1] == " ")
+            {
+                board[Int16.Parse(userInput) - 1] = curentPlayer;
+            }
+            else
+            {
+                userSelectSquare();
+            }
+
+        }
+
+        private bool oneToNine(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '1' || c > '9')
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ I AM SPEED (3 på rad EDITION)/Program.cs
@@ -0,0 +1,13 @@
+﻿using I_AM_SPEED__3_på_rad_EDITION_.Model;
+
+namespace I_AM_SPEED__3_på_rad_EDITION_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game Game = new Game();
+            Game.StartGame();
+        }
+    }
+}
